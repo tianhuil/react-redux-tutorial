77d693f7f8fa107dd5807919ef982ecde5c1a16c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-01-06 10:57:33 -0500

    Some progress

--- README.md
+++ README.md
@@ -16,3 +16,8 @@ Valentino Gagliardi - [valentinog.com](https://www.valentinog.com) - valentino@v
 ## License
 
 This project is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
+
+# Following along with this tutorial
+https://www.valentinog.com/blog/react-redux-tutorial-beginners/
+
+But quasi giving up
---
+++ src/js/components/Form.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import uuidv1 from "uuid";
+import { addArticle } from "../actions/index";
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addArticle: article => dispatch(addArticle(article))
+  };
+};
+
+class ConnectedForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const {title} = this.state;
+    const id = uuidv1();
+    this.props.addArticle({ title, id });
+    this.setState({title: ""});
+  }
+
+  render() {
+    const {title} = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <iv className
+    )
+  }
+}
